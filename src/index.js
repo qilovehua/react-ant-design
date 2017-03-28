88d5381c801157cb2918063f510bc765f2c5fbdf
linhq <linhq@linhqs-MacBook-Pro.local>
2017-03-28 16:31:32 +0800

    add react-router2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, IndexRoute, browserHistory, Link, History} from 'react-router';
 import App from './App';
 import '../public/main.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render((
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={App}/>
+                {/*<Route path="/login" component={Login}/>*/}
+            </Route>
+        </Router>
+    ), document.getElementById('root')
 );
