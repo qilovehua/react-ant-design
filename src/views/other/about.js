579b1b07f806ee44d97e5ddc8c772800271d9431
linhq <linhq@linhqs-MacBook-Pro.local>
2017-03-30 17:25:41 +0800

    备注less

--- src/views/other/about.js
+++ src/views/other/about.js
@@ -17,6 +17,7 @@ class About extends Component {
                     <li>React-Router</li>
                     <li>ant-design</li>
                     <li>Jquery</li>
+                    <li>Less - 未使用webpack打包，使用WebStorm自动编译生成CSS文件</li>
                     <li>CSS(3)</li>
                     <li>HTML(5)</li>
                     <li>Fetch, JSONP 跨域访问API获取数据</li>
@@ -26,6 +27,11 @@ class About extends Component {
                 <h3>获取地址：
                     <a href="https://github.com/qilovehua/react-ant-design" target="_blank">https://github.com/qilovehua/react-ant-design</a>
                 </h3>
+                <br/>
+                <br/>
+                <h3>备注: </h3>
+                <p>1. 需要使用lessc，如未安装请使用'npm install -g less'</p>
+                <p>2. <a href="http://www.cnblogs.com/fxair/p/3919674.html" target="_black">用webstorm自动编译less产出css和sourcemap</a></p>
             </div>
         );
     }
