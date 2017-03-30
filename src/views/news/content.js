a1bb7c383ceda56b1cfa49e1f8cbccda9c430179
linhq <linhq@linhqs-MacBook-Pro.local>
2017-03-30 11:11:50 +0800

    有content时显示按钮，点击后显示详情

--- src/views/news/content.js
+++ src/views/news/content.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react';
 
+import $ from 'jquery';
+import _ from 'lodash';
 import { Card, Icon } from 'antd';
 
 class Content extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            show: false,
+            showOpt: true,
+        }
     }
 
-    componentDidMount(){
+    componentWillMount(){
+        var {content} = this.props;
+        if(!content || !content.content){
+            this.setState({
+                showOpt: false,
+            });
+        }
+    }
+
+    componentWillReceiveProps(props){
+        if(!_.isEqual(props.content, this.props.content)){
+            this.setState({
+                show: false,
+                showOpt: props.content && props.content.content,
+            });
+        }
     }
 
     unInterested(news_id){
         this.props.unInterested && this.props.unInterested(news_id);
     }
 
+    showDetail(show){
+        console.log('==show', show);
+        this.setState({
+            show,
+        }, ()=>{
+            if(show){
+                var {content} = this.props;
+                if(content && _.size(content.content)) {
+                    var className = "content-detail" + content.news_id;
+                    $('.'+className).html(content.content);
+                }
+            }
+        });
+    }
+
     render() {
         var {content} = this.props;
+        var {show, showOpt} = this.state;
         var existImage = !!content.text_image0;
         return (
             <div className="content">
@@ -31,6 +68,23 @@ class Content extends Component {
                         <a className="content-title" href="javascript:void(0)">
                             {content.title}
                         </a>
+                        {
+                            !show &&
+                                <div className="content-opt" onClick={()=> {this.showDetail(!show)}}>
+                                    <span style={{fontSize: 13, color: '#777'}}>{content.digest}</span>
+                                    {showOpt && ' 展开详情'}
+                                </div>
+                        }
+                        {
+                            show && showOpt &&
+                                <div className={"content-detail" + content.news_id}/>
+                        }
+                        {
+                            show && showOpt &&
+                                <div className="content-opt" onClick={()=> {this.showDetail(!show)}}>
+                                    {' 关闭详情'}
+                                </div>
+                        }
                         <div className="content-sub-title">
                             <div>{'来源: ' + content.source + ' ⋅ ' + content.reply_count + '评论 ⋅ ' + content.edit_time}</div>
                             <div onClick={()=>{this.unInterested(content.news_id)}}><Icon type="close" style={{fontSize: 15}}/></div>
