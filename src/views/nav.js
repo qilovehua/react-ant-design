3c22c42f7e7b507d866d3f2d4686da3075505be9
linhq <linhq@linhqs-MacBook-Pro.local>
2017-05-11 14:56:35 +0800

    nav导航栏 改用icon 点击时弹出下拉菜单

--- src/views/nav.js
+++ src/views/nav.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
 import logo from './../../public/image/logo.svg';
-import {Layout, Menu, BackTop} from 'antd';
+import {Layout, Menu, BackTop, Icon, Dropdown} from 'antd';
 const {Header} = Layout;
+
+var menus = ['新闻头条', '首页', '致谢', '关于'];
 
 class Nav extends Component {
 
@@ -24,6 +26,16 @@ class Nav extends Component {
     }
 
     render() {
+        var menu = (
+            <Menu
+                theme="dark"
+                defaultSelectedKeys={[this.state.key]}
+            >
+                <Menu.Item key="1"><Link to="/#1">{menus[1]}</Link></Menu.Item>
+                <Menu.Item key="2"><Link to="/thanks#2">{menus[2]}</Link></Menu.Item>
+                <Menu.Item key="3"><Link to="/about#3">{menus[3]}</Link></Menu.Item>
+            </Menu>
+        );
         return (
             <div>
                 <div style={{position: 'fixed', width: '100%', zIndex: 100, top: 0}}>
@@ -33,7 +45,7 @@ class Nav extends Component {
                                 <img src={logo} className="App-logo" alt="logo" />
                             </div>
                             <div className="nav-title">
-                                新闻头条
+                                {menus[0]}
                             </div>
                             <div className="nav-menu">
                                 <Menu
@@ -42,10 +54,15 @@ class Nav extends Component {
                                     defaultSelectedKeys={[this.state.key]}
                                     style={{ lineHeight: '64px' }}
                                 >
-                                    <Menu.Item key="1"><Link to="/#1">首页</Link></Menu.Item>
-                                    <Menu.Item key="2"><Link to="/thanks#2">致谢</Link></Menu.Item>
-                                    <Menu.Item key="3"><Link to="/about#3">关于</Link></Menu.Item>
+                                    <Menu.Item key="1"><Link to="/#1">{menus[1]}</Link></Menu.Item>
+                                    <Menu.Item key="2"><Link to="/thanks#2">{menus[2]}</Link></Menu.Item>
+                                    <Menu.Item key="3"><Link to="/about#3">{menus[3]}</Link></Menu.Item>
                                 </Menu>
+                            </div>
+                            <div className="nav-menu-mobile">
+                                <Dropdown overlay={menu}>
+                                    <Icon type="bars"/>
+                                </Dropdown>
                             </div>
                         </Header>
                     </Layout>
